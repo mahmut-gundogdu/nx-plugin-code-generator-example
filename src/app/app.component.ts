bac89f282bbc8e3111abf1fcabef2340209416a1
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2023-05-09 19:11:26 +0300

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contoso-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'contoso-app';
+}
